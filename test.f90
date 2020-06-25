1548457490b00ac280f234f50b39a9fe0f2beb73
Alexander Robinson <robinson@ucm.es>
2020-06-25 16:47:53 +0200

    Brute force method almost working.

--- test.f90
+++ test.f90
@@ -34,6 +34,7 @@ program ocean
     allocate(xc(nx))
     allocate(yc(ny)) 
     allocate(f_grnd(nx,ny))
+    allocate(mask_ocn(nx,ny))
     allocate(mask_ocn_ref(nx,ny))
 
     ! Load domain dimensions from input file 
@@ -43,20 +44,104 @@ program ocean
     ! Load f_grnd from file 
     call load_f_grnd(f_grnd,file_in)
 
+    ! Add a few holes in f_grnd that are not connected to the ocean 
+    ! for testing the algorithm 
+    f_grnd(130:135,80:82)   = 0.0 
+    f_grnd(110:112,130:137) = 0.0 
+    
     ! Define reference ocean mask: this sets the 
     ! seed location(s) of where we know open ocean exists 
     call define_ocn_ref(mask_ocn_ref,xc,yc,domain="Antarctica")
     
-    mask_ocn = f_grnd 
+    ! Determine open-ocean mask for our domain 
+    call find_ocean(mask_ocn,f_grnd,mask_ocn_ref)
 
     ! Write output to file 
-    call write_ocn(file_out,file_in,mask_ocn_ref,mask_ocn,xc,yc)
+    call write_ocn(file_out,file_in,f_grnd,mask_ocn_ref,mask_ocn,xc,yc)
 
 contains 
 
-subroutine find_ocean()
+subroutine find_ocean(mask,f_grnd,mask_ref)
+    ! Brute-force routine to find all ocean points 
+    ! (ie, when f_grnd < 1) connected to
+    ! the open ocean as defined in mask_ref. 
 
     implicit none 
+
+    integer,    intent(OUT) :: mask(:,:) 
+    real(prec), intent(IN)  :: f_grnd(:,:)
+    integer,    intent(IN)  :: mask_ref(:,:) 
+
+    ! Local variables 
+    integer :: i, j, q, nx, ny
+    integer :: im1, ip1, jm1, jp1 
+    integer :: n_unfilled 
+
+    integer    :: mask_neighb(4) 
+
+    integer, parameter :: qmax = 1000 
+
+    nx = size(mask,1)
+    ny = size(mask,2) 
+
+    ! First specify land points (mask==0)
+    ! and assume all floating points are 'closed ocean' points (mask==-1)
+    mask = 0 
+    where (f_grnd .lt. 1.0) mask = -1 
+
+    ! Now populate our ocean mask to be consistent 
+    ! with known open ocean points 
+    where (mask .eq. -1 .and. mask_ref .eq. 1) mask = 1 
+
+if (.TRUE.) then 
+    ! Iteratively fill in open-ocean points that are found
+    ! next to known open-ocean points
+    do q = 1, qmax 
+
+        n_unfilled = 0 
+
+        do j = 1, ny 
+        do i = 1, nx 
+
+            ! Get neighbor indices 
+            im1 = max(i-1,1)
+            ip1 = min(i+1,nx)
+            jm1 = max(j-1,1)
+            jp1 = min(j+1,ny)
+            
+            if (mask(i,j) .eq. 1) then 
+                ! This is an open-ocean point 
+
+                mask_neighb   = [mask(im1,j),mask(ip1,j),mask(i,jm1),mask(i,jp1)]
+
+                if (count(mask_neighb .eq. -1) .gt. 0) then 
+                    ! An ocean neighbor exists
+
+                    ! Log additional case found on this iteration
+                    n_unfilled = n_unfilled + 1
+
+                    ! Define any neighbor ocean points as open-ocean points
+                    if (mask(im1,j) .eq. -1) mask(im1,j) = 1 
+                    if (mask(i,jm1) .eq. -1) mask(i,jm1) = 1 
+                    if (mask(i,jp1) .eq. -1) mask(i,jp1) = 1 
+                    
+                    !if (mask(ip1,j) .eq. -1) mask(ip1,j) = 1 
+                    
+                end if 
+
+            end if 
+
+        end do 
+        end do  
+
+        write(*,*) q, n_unfilled 
+
+        ! Exit loop if no more open-ocean points are found 
+        if (n_unfilled .eq. 0) exit 
+
+    end do 
+
+end if 
 
     return 
 
@@ -160,14 +245,15 @@ subroutine define_ocn_ref(mask,xc,yc,domain)
 
     return 
 
-end subroutine define_ocn_ref 
+end subroutine define_ocn_ref
 
-subroutine write_ocn(filename,file_in,mask_ref,mask,xc,yc)
+subroutine write_ocn(filename,file_in,f_grnd,mask_ref,mask,xc,yc)
 
     implicit none 
 
     character(len=512), intent(IN)  :: filename 
     character(len=512), intent(IN)  :: file_in  
+    real(prec),         intent(IN)  :: f_grnd(:,:) 
     integer,            intent(IN)  :: mask_ref(:,:)
     integer,            intent(IN)  :: mask(:,:) 
     real(prec),         intent(IN)  :: xc(:) 
@@ -178,6 +264,7 @@ subroutine write_ocn(filename,file_in,mask_ref,mask,xc,yc)
     call nc_write_dim(filename,"xc",x=xc)
     call nc_write_dim(filename,"yc",x=yc)
     
+    call nc_write(filename,"f_grnd",  f_grnd,  dim1="xc",dim2="yc") 
     call nc_write(filename,"mask_ref",mask_ref,dim1="xc",dim2="yc") 
     call nc_write(filename,"mask",    mask,    dim1="xc",dim2="yc") 
 
